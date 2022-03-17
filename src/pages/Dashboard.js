3cd643ec5c0124f924dc32bf42f7f3df5b16c0ef
Charles Kasasira <charleskasasira01@gmail.com>
2022-03-17 21:05:14 +0300

    coursePastPapers

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,32 +1,20 @@
 import '../styles/dashboard.css'
 
+import { courseUnit } from '../helpers/courseUnits'
+
 function Dashboard() {
   return (
     <>
     <div>Welcome Charles</div>
 
     <div className='status'>
-      <div className='status-item'>
-        <span>CSC 1100</span>
-        <h4>Digital Innovation And Computational Thinking</h4>
+      {courseUnit.map(unit => (
+        <div className='status-item'>
+        <span>{unit.code}</span>
+        <h4>{unit.name}</h4>
 
       </div>
-      <div className='status-item'>
-        <span>CSC 1100</span>
-        <h4>Computer Literacy</h4>
-      </div>
-      <div className='status-item'>
-        <span>CSC 1100</span>
-        <h4>Computer Architecture And Organisation</h4>
-      </div>
-      <div className='status-item'>
-        <span>CSC 1100</span>
-        <h4>Mathematics for Computer Science</h4>
-      </div>
-      <div className='status-item'>
-        <span>CSC 1100</span>
-        <h4>Structure and Object-Oriented Programming</h4>
-      </div>
+      ))}
     </div>
 
     <div className='graph'>
