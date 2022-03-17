c245d470cd71d09a2633b11922d75b58cb2fcf5b
Charles Kasasira <charleskasasira01@gmail.com>
2022-03-17 18:25:09 +0300

    working on the dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import '../styles/dashboard.css'
 
 function Dashboard() {
   return (
+    <>
     <div>Welcome to the Dashboard</div>
+
+    <div className='status'>
+      <div className='status-item'>
+        <span>CSC 1100</span>
+        <h4>Digital Innovation And Computational Thinking</h4>
+
+      </div>
+      <div className='status-item'>
+        <span>CSC 1100</span>
+        <h4>Computer Literacy</h4>
+      </div>
+      <div className='status-item'>
+        <span>CSC 1100</span>
+        <h4>Computer Architecture And Organisation</h4>
+      </div>
+      <div className='status-item'>
+        <span>CSC 1100</span>
+        <h4>Mathematics for Computer Science</h4>
+      </div>
+      <div className='status-item'>
+        <span>CSC 1100</span>
+        <h4>Structure and Object-Oriented Programming</h4>
+      </div>
+    </div>
+    </>
   )
 }
 
