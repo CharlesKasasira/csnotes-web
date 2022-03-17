a66e328ae9b98f0e68166822ecf26db87d07979c
Charles Kasasira <charleskasasira01@gmail.com>
2022-03-17 19:45:43 +0300

    coursePastPapers

--- src/helpers/courseUnits.js
+++ src/helpers/courseUnits.js
@@ -3,7 +3,7 @@
 
 const courseUnit = [
     {
-        code: 'CSC 1100',
+        code: 'CSC 1109',
         name: 'Digital Innovation and Computational Thinking',
         link: '/digital-innovation-and-computational Thinking/past-papers'
     },
@@ -13,22 +13,22 @@ const courseUnit = [
         link: '/computer-literacy/past-papers'
     },
     {
-        code: 'CSC 1100',
+        code: 'CSC 1104',
         name: 'Computer Architecture and Organisation',
         link: '/computer-architecture-and-organisation/past-papers'
     },
     {
-        code: 'CSC 1100',
+        code: 'CSC 1102',
         name: 'Structured and Object-oriented Programming',
         link: '/structured-and-object-oriented-programming/past-papers'
     },
     {
-        code: 'CSC 1100',
+        code: 'CSK 1101',
         name: 'Communication Skills for IT',
         link: '/communication-skills/past-papers'
     },
     {
-        code: 'CSC 1100',
+        code: 'CSC 1105',
         name: 'Mathemeatics for Computer Science',
         link: '/mathematics-for-computer-science/past-papers'
     },
